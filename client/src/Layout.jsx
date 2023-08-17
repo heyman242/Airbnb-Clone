d981275e8f42e272986d988ebfb7a202181ca778
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-17 22:27:43 +0530

    frontend basic route setup header and outlet for the app

---
+++ client/src/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from './Header'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <div className='p-4'>
+        <Header/>
+        <Outlet/>
+    </div>
+  )
+}
+
+export default Layout
